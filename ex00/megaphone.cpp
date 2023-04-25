11f80c68ea8b6d5ef6d5d7233acd1460cb2b1b4c
Naomi Roth <nroth@c2r6s5.42prague.com>
2023-04-25 20:59:44 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
+	argv++;
+	while (*argv)
+	{
+		i = 0;
+		while ((*argv)[i])
+		{
+			if ((*argv)[i] >= 'a' && (*argv)[i] <= 'z')
+				(*argv)[i] -= 32;
+			i++;
+		}
+		std::cout << *argv;
+		std::cout << " ";
+		argv++;
+	}
+	std::cout << "\n";
+	return (0);
+}
