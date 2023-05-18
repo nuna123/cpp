e37daedca6f95afc610958efbabf89a9e47fe025
Naomi Roth <nroth@c2r6s5.42prague.com>
2023-05-18 16:12:37 +0200

    cpp01 - ex01

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "string address: " << &string << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+
+	std::cout << "\nstring value: " << string << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+	return (0);
+}
