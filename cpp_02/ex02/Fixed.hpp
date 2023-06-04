800b84057b8209dd1254ccd91d377ac30fce3240
Naomi Roth <nroth@c2r4s5.42prague.com>
2023-06-04 15:44:46 +0200

    cpp 02 - ex02 - incoplete

---
+++ cpp_02/ex02/Fixed.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+# include <iostream>
+#include <math.h>
+
+class	Fixed
+{
+	private:
+		int					value;
+		static const int	fractional_bits;
+
+	public:
+		Fixed (); //Default constructor
+		Fixed (const Fixed &fixed); //copy constructor
+		~Fixed (); // Destructor
+		Fixed &operator = (Fixed const &fixed_copy); //Copy assignment operator
+
+		Fixed (const int);
+		Fixed (const float);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+
+		//COMPARISON OPERATORS
+ 		bool operator>(Fixed const& fixed);
+		bool operator<(Fixed const& fixed);
+		bool operator<=(Fixed const& fixed);
+		bool operator>=(Fixed const& fixed);
+		bool operator==(Fixed const& fixed);
+		bool operator!=(Fixed const& fixed);
+
+		//ARITHMATIC OPERATORS
+		Fixed operator+(Fixed const& fixed);
+		Fixed operator-(Fixed const& fixed);
+		Fixed operator*(Fixed const& fixed);
+		Fixed operator/(Fixed const& fixed);
+
+};
+		std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+#endif
