2839e7932f3ad4dd022973cf4cccb3b78b1f021e
Naomi Roth <nroth@c2r6s7.42prague.com>
2023-12-14 15:03:24 +0100

    cpp08 ex02:

--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -6,23 +6,25 @@ int main(){
 
 	mstack.push(5);
 	mstack.push(17);
+
 	std::cout << mstack.top() << std::endl;
+
 	mstack.pop();
 	std::cout << mstack.size() << std::endl;
+
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it; 
-	while (it != ite)
+
+	MutantStack<int>::iterator start_it = mstack.begin();
+	MutantStack<int>::iterator end_it = mstack.end();
+	++start_it;
+	--start_it;
+	while (start_it != end_it)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *start_it << std::endl;
+		++start_it;
 	}
-	std::stack<int> s(mstack); 
 	return 0;
 }
